0438fd10884fa487d3c4cb23f93cdc015d2bb3c0
Avi Kohn <AMKohn@users.noreply.github.com>
2020-03-24 16:55:41 -0400

    Updated container class names

--- frontend/src/Course.js
+++ frontend/src/Course.js
@@ -3,7 +3,7 @@ import {Link} from "react-router-dom";
 
 export default function Course() {
 	return (
-		<div className={"dashboard"}>
+		<div className={"course"}>
 			<h1>Course</h1>
 			<Link to="/module">Go to the module page</Link>
 		</div>
--- frontend/src/Module.js
+++ frontend/src/Module.js
@@ -2,7 +2,7 @@ import React from "react";
 
 export default function Module() {
 	return (
-		<div className={"dashboard"}>
+		<div className={"module"}>
 			<h1>Module</h1>
 			<p>This is the module page</p>
 		</div>
