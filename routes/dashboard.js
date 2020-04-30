9d6a1c88d41470a3660c65df356457e4c4c04581
Avi Kohn <AMKohn@users.noreply.github.com>
2020-04-30 10:39:56 -0400

    Made the dashboard API live
    This includes some schema changes

--- routes/dashboard.js
+++ routes/dashboard.js
@@ -1,77 +1,70 @@
+const Progress = require("../lib/db/Progress");
+
 module.exports = (req, res) => {
-	if (req.isAuthed) {
-		console.log("Got dashboard request for user", req.username);
+	if (!req.isAuthed) {
+		return res.status(401).json({ error: { message: "You must be signed in to view the dashboard" } });
 	}
 
-	// Get user from DB, use mongoose .populate() to load course info.
-	// Generate output data. Need to add a timeEstimate to modules to be able to calculate course time
-	// Sort by In Progress -> Not Started -> Completed
+	Progress
+		.find({ user: req.username })
+		.sort({ status: -1 })
+		.populate("course")
+		.lean()
+		.exec((err, progress) => {
+			progress = progress || [];
 
-	res.json({
-		courses: [{
-			id: 1,
-			name: "Intro to Python",
-			instructor: "John Doe, PhD",
-			status: "inprogress",
+			if (err) {
+				return res.status(500).json({ error: { message: "Failed to get courses" } });
+			}
 
-			timeRemaining: 2,
-			totalTime: 10,
-			progressPercentage: 20, // % of modules completed, not time remaining
-			moduleCount: 37,
-			continueLink: "/courses/1/modules/101"
-		}, {
-			id: 2,
-			name: "Network and Data Communication",
-			instructor: "John Doe, PhD",
-			status: "inprogress",
+			let courses = [];
 
-			timeRemaining: 6,
-			totalTime: 14,
-			progressPercentage: 43,
-			moduleCount: 24,
-			continueLink: "/courses/2/modules/213"
-		}, {
-			id: 3,
-			name: "Advanced Python",
-			instructor: "John Doe, PhD",
-			status: "new",
+			progress.forEach(p => {
+				let ret = {
+					id: p.course._id,
+					name: p.course.name,
+					instructor: p.course.instructor,
+					status: p.status
+				};
 
-			timeRemaining: 11,
-			totalTime: 11,
-			progressPercentage: 0,
-			moduleCount: 33,
-			continueLink: "/courses/3/modules/301"
-		}, {
-			id: 4,
-			name: "React and Modern Web Frameworks",
-			instructor: "John Doe, PhD",
-			status: "new",
+				let totalTime = 0;
+				let completedTime = 0;
 
-			timeRemaining: 7,
-			totalTime: 7,
-			progressPercentage: 0,
-			moduleCount: 18,
-			continueLink: "/courses/4/modules/401"
-		}, {
-			id: 5,
-			name: "Web Development",
-			instructor: "John Doe, PhD",
-			status: "completed",
+				let totalModules = 0;
+				let completedModules = 0;
 
-			timeRemaining: 0,
-			totalTime: 9,
-			progressPercentage: 100,
-			moduleCount: 27
-		}, {
-			id: 6,
-			name: "Fundamentals of Computing",
-			instructor: "John Doe, PhD",
-			status: "completed",
+				p.course.lessons.forEach(l => l.modules.forEach(m => {
+					totalModules++;
 
-			timeRemaining: 0,
-			totalTime: 16,
-			progressPercentage: 100,
-			moduleCount: 41
-		}]
-	});
+					// Hardcoded 7 minute-per-module time estimate
+					totalTime += 7;
+
+					if (p.completedModules.indexOf(m) !== -1) {
+						completedModules++;
+						completedTime += 7;
+					}
+					else if (!ret.continueLink) {
+						ret.continueLink = `/courses/${p.course._id}/modules/${m}`
+					}
+				}));
+
+				ret.timeRemaining = totalTime - completedTime;
+				ret.totalTime = totalTime;
+				ret.progressPercentage = Math.round((completedModules / totalModules) * 100);
+				ret.moduleCount = totalModules;
+
+				if (!ret.continueLink) {
+					ret.continueLink = `/courses/${p.course._id}/modules/${p.course.lessons[0].modules[0]}`;
+				}
+
+				if (ret.status === "inprogress") {
+					courses.unshift(ret);
+				}
+				else {
+					courses.push(ret);
+				}
+			});
+
+			res.json({ courses });
+		});
 };
